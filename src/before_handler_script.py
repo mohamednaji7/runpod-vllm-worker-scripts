58762a246bd1cfad4afe39418050f91c5286f0b3
Mohamed Nagy <69568400+mohamednaji7@users.noreply.github.com>
2025-01-22 07:04:36 +0200

    Create before_handler_script.py

---
+++ src/before_handler_script.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+import subprocess
+import logging
+
+
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
+def before_handler_script():
+    print("I will run before the handler")
+    SCRIPT_REPO_DIR = "./before_handler_script_repo"
+    SCRIPT_SRC_DIR = f"{SCRIPT_REPO_DIR}/src"
+    LOCAL_SCRIPT_DIR = f"{os.getcwd()}/"
+
+    # Ensure the local SCRIPT directory exists before copying files
+    if not os.path.exists(LOCAL_SCRIPT_DIR):
+        os.makedirs(LOCAL_SCRIPT_DIR)
+
+    # Now copy the files
+    for item in os.listdir(SCRIPT_SRC_DIR):
+        src_path = os.path.join(SCRIPT_SRC_DIR, item)
+        dest_path = os.path.join(LOCAL_SCRIPT_DIR, item)
+        
+        if os.path.isdir(src_path):
+            shutil.copytree(src_path, dest_path)
+        else:
+            # Ensure the destination directory exists before copying
+            if not os.path.exists(os.path.dirname(dest_path)):
+                os.makedirs(os.path.dirname(dest_path))
+            shutil.copy2(src_path, dest_path)
+
+    logging.info("before_handler_script copying completed successfully")
