27ff8d208d0bceb0c6c11f608d3cfd53b49f90cc
mohamednaji7 <n4jidx@gmail.com>
2025-01-23 22:31:04 +0200

    keep_try_update_and_run

--- src/update_run_keep_try.py
+++ src/update_run_keep_try.py
@@ -2,50 +2,38 @@ import os
 import subprocess
 import time
 
-def update_run_keep_try(scriptname):
-    # Try to run the first time regardless of KEEP_TRY or KEEP_RUN
+def update():
+    # Perform git pull to update
+    subprocess.run(['git', 'pull'], check=True)
+    print("Git pull successful.")
+    
+def run(scriptname):
+    # Run the specified script
+    print(f"Running {scriptname}...")
+    subprocess.run(['python3', scriptname], check=True)
+    print(f"Script {scriptname} executed successfully.")
+
+def update_and_run(scriptname):
+    update
+    run(scriptname)
+
+
+def try_update_and_run(scriptname):
     try:
-        # Perform git pull to update
-        subprocess.run(['git', 'pull'], check=True)
-        print("Git pull successful.")
-        
-        # Run the specified script
-        subprocess.run(['python3', scriptname], check=True)
-        print(f"Script {scriptname} executed successfully.")
-    except subprocess.CalledProcessError as e:
+        update_and_run(scriptname)
+    except Exception as e:
         print(f"Error occurred: {e}")
         print("Exiting after first failure.")
 
-    # If KEEP_TRY is set to "True", continue retrying
-    while os.getenv('KEEP_TRY') == 'True' or os.getenv('KEEP_RUN') == 'True':
-        if os.getenv('KEEP_TRY') == 'True':
-            print("KEEP_TRY is set. Retrying...")
-        elif os.getenv('KEEP_RUN') == 'True':
-            print("KEEP_RUN is set. Continuing execution...")
+def keep_try_update_and_run(scriptname):
+    try_update_and_run(scriptname)
+    while os.getenv('KEEP_TRY') == 'True' or os.getenv('KEEP_RUN') == 'True':   
+        print(f"KEEP_TRY: {os.getenv('KEEP_TRY')}\nKEEP_RUN: {os.getenv('KEEP_RUN')}")
+        try_update_and_run(scriptname)
 
-        try:
-            # Perform git pull to update
-            subprocess.run(['git', 'pull'], check=True)
-            print("Git pull successful.")
-            
-            # Run the specified script
-            subprocess.run(['python3', scriptname], check=True)
-            print(f"Script {scriptname} executed successfully.")
-            
-            # If KEEP_RUN is not set, break out of the loop
-            if os.getenv('KEEP_RUN') != 'True':
-                break  # Exit the loop if script runs successfully and KEEP_RUN is not set
-            
-        except subprocess.CalledProcessError as e:
-            print(f"Error occurred: {e}")
-            print("Retrying in 5 seconds...")
-            time.sleep(5)  # Wait for 5 seconds before retrying
-    
-    else:
-        if os.getenv('KEEP_TRY') != 'True' and os.getenv('KEEP_RUN') != 'True':
-            print("Neither KEEP_TRY nor KEEP_RUN environment variables are set. Exiting.")
-        else:
-            print("Exiting after reaching exit condition for KEEP_RUN.")
+def main():
+    scriptname  = 'main.py'
+    keep_try_update_and_run(scriptname)
 
-scriptname  = 'main.py'
-update_run_keep_try(scriptname)
+if __name__ == "__main__":
+    main()
