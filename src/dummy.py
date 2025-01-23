594dad8cd8df30cdb8c9970e3f18ab7cc1ac24da
mohamednaji7 <n4jidx@gmail.com>
2025-01-23 18:48:47 +0200

    addin logging for errors

--- src/dummy.py
+++ src/dummy.py
@@ -1,9 +1,17 @@
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def dummy():
     logging.info("hello RunPod! `dummy` script is here.")
 
 def main():
-    dummy()
+    try:
+        dummy()
+    except Exception as e:
+        logging.error(f'ERROR in `dummy.py` {e}')
   
 if __name__ == "__main__":
     main()
