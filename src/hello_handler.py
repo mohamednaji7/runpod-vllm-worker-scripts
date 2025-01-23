955ce6732855430c1a0c137bfc1410b6df0347da
mohamednaji7 <n4jidx@gmail.com>
2025-01-23 23:50:13 +0200

    hello_handler

--- src/hello_handler.py
+++ src/hello_handler.py
@@ -3,13 +3,22 @@ import subprocess
 import os
 # If your handler runs inference on a model, load the model here.
 # You will want models to be loaded into memory before starting serverless.
+import logging
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logging.debug("hello RunPod! 'hello_handler.py' script is here.")
+
 
 
 def update():
     # Perform git pull to update
     subprocess.run(['git', 'pull'], check=True)
-    print("Git pull successful.")
-    
+    logging.debug("Git pull successful.")
+
 def handler(job):
     """ Handler function that will be used to process jobs. """
     if os.getenv('PULL_BEFORE_REQUEST')=='True':
