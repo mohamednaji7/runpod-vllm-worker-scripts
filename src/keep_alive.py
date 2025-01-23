20982ed2366edcae5ea7ba5883e272e16ef1a954
mohamednaji7 <n4jidx@gmail.com>
2025-01-23 21:44:26 +0200

    adding the handler

--- src/keep_alive.py
+++ src/keep_alive.py
@@ -1,3 +1,21 @@
 from install import  install_requirements 
 
 install_requirements("requirements.txt", verbose=True)
+
+
+import runpod
+
+# If your handler runs inference on a model, load the model here.
+# You will want models to be loaded into memory before starting serverless.
+
+
+def handler(job):
+    """ Handler function that will be used to process jobs. """
+    job_input = job['input']
+
+    name = job_input.get('name', 'World')
+
+    return f"Hello, {name}!"
+
+
+runpod.serverless.start({"handler": handler})
