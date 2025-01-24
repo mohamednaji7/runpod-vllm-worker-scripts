9fa0e8d979ea665f5d5c0b1deff62e9a831f926c
mohamednaji7 <n4jidx@gmail.com>
2025-01-24 06:59:55 +0200

    init

--- src/setup.sh
+++ src/setup.sh
@@ -5,7 +5,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b
 
 ~/miniconda3/bin/conda init
-source ~/.bashrc
+# source ~/.bashrc
 
 
 conda create --name unsloth_env python=3.10 -y
