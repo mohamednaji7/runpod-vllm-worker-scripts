70b2b9294fed365aeb3ccd4e576553b9b422c4b7
mohamednaji7 <n4jidx@gmail.com>
2025-01-24 10:08:05 +0200

    main

--- deployment/main.py
+++ deployment/main.py
@@ -1,13 +1,41 @@
-logging 
-rich_console.info(f"Running `update` {{git, pull}}...")
-ho to run setup.sh
-# Perform git pull to update
-res = subprocess.run(['git', 'pull'], check=True)
-rich_console.info(res)
+import os
+import subprocess
+import sys
 
-rich_console.info(f"Running `{scriptname}`...")
-# Run the specified script
-result = subprocess.run(['python3', scriptname], check=True)
+if os.environ.get('SCRIPT_NAME') is not None:
+    import logging
+    # Configure logging to output plain text to stdout
+    logging.basicConfig(
+        level=logging.DEBUG,       # Set the minimum logging level
+        format='[%(levelname)s] %(message)s'  # Text-only format
+    )
+    rich_console = logging
 
-# Log the output
-rich_console.info(result)
+else:
+    from rich_console import Rich_Console
+    rich_console = Rich_Console()
+
+def main():
+    # Git Pull
+    rich_console.info("Running `update` {git, pull}...")
+    res = subprocess.run(['git', 'pull'], check=True)
+    rich_console.info(res)
+
+    # Run setup.sh
+    rich_console.info("Running setup.sh...")
+    subprocess.run(['bash', 'setup.sh'], check=True)
+
+    # Change conda environment
+    rich_console.info("Activating unsloth_env...")
+    os.system("conda activate unsloth_env")
+
+    # Run handler.py
+    scriptname = 'handler.py'
+    rich_console.info(f"Running `{scriptname}`...")
+    result = subprocess.run(['python3', scriptname], check=True)
+
+    # Log the output
+    rich_console.info(result)
+
+if __name__ == "__main__":
+    main()
