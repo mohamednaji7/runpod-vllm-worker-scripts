445ff7d34d5ecb3181a1a607b136606dbbba1387
mohamednaji7 <n4jidx@gmail.com>
2025-01-26 13:50:44 +0200

    u

--- empty_test/handler.py
+++ empty_test/handler.py
@@ -4,13 +4,17 @@ import runpod
 def handler(job):
     """ Handler function that will be used to process jobs. """
 
-
     job_input = job['input']
     if 'openai_input' in  job_input:
         job_input = job_input['openai_input']
 
-    return  {"output": {"route": f"Receveid on the route {job_input.get('openai_route', 'Not openai_route')}",
-                        "input": job_input}}
+    if 'messages' in job_input:
+        input_to_process = job_input.get('messages')
+    elif 'prompt' in job_input:
+        input_to_process = job_input.get("prompt")
 
+    return  {"route": f"Receveid on route {job_input.get('openai_route', 'other than openai_route')}",
+             "input_to_process": input_to_process,
+             "input": job_input}
 
 runpod.serverless.start({"handler": handler})
