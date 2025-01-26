eb9042c7b0214c3c9ba54e617ed4846f72a5f26a
mohamednaji7 <n4jidx@gmail.com>
2025-01-26 14:36:52 +0200

    u

--- empty_test/test.sh
+++ empty_test/test.sh
@@ -3,7 +3,13 @@
 
 # Display the RunPod endpoint and API key
 
-echo "ENDPOINT ID: $ENDPOINT_ID"
+# echo "ENDPOINT ID: $ENDPOINT_ID"
+
+# echo "RunPod Endpoint: $RUNPOD_ENDPOINT_openai_URL"
+# echo "RunPod Endpoint: $RUNPOD_ENDPOINT_run_URL"
+# echo "RunPod Endpoint: $RUNPOD_ENDPOINT_runsync_URL"
+
+# echo "RunPod API Key: $RUNPOD_API_KEY"
 
 # Make a cURL request to RunPod API
 curl "$RUNPOD_ENDPOINT_openai_URL" \
