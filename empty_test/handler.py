87d6bc9cc590259f1872e5482a5ee5281f3b0f07
mohamednaji7 <n4jidx@gmail.com>
2025-01-26 13:44:22 +0200

    u

--- empty_test/handler.py
+++ empty_test/handler.py
@@ -9,7 +9,7 @@ def handler(job):
     if 'openai_input' in  job_input:
         job_input = job_input['openai_input']
 
-    return  {"output": {"route": f"Receveid on the route {job_input.get('openai_route', "Not openai_route")}",
+    return  {"output": {"route": f"Receveid on the route {job_input.get('openai_route', 'Not openai_route')}",
                         "input": job_input}}
 
 
